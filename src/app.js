5b6884759bf4820847ce4133d325d12c29965a01
JackCaldwellNichols <jack.wcn@gmail.com>
2022-08-27 05:12:07 +0000

    My first push

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onclick = () => {
+  document.querySelector("#the_excuse").innerHTML = generateExcuse();
+  console.log("hello from the console");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My web developer", "His friendly horse", "My bird"];
+  let action = [" ate", " peed on", " crushed", " broke"];
+  let what = [" my homework", " the keys", " the car", " my bed"];
+  let when = [
+    " before the class",
+    " before I finished",
+    " during my lunch",
+    " while I was sleeping"
+  ];
+
+  let result =
+    who[Math.floor(Math.random() * who.length)] +
+    action[Math.floor(Math.random() * action.length)] +
+    what[Math.floor(Math.random() * what.length)];
+
+  return result;
 };
